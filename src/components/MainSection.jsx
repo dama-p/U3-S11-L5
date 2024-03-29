2dc1a82bac323e37b9c74c91fa37c3ea8aa3417b
Damaride Putignano <damarideputignano@gmail.com>
2024-03-29 15:08:01 +0100

    Added fetch & redux

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,50 +1,225 @@
-import { Row } from "react-bootstrap";
-import AlbumList from "./AlbumList";
-import FetchAlbumsData from "./FetchAlbumsData";
-import { useSelector } from "react-redux";
-
+import { Col, Row } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 const MainSection = () => {
-  let rockArtists = [
-    'queen',
-    'arcticmonkeys',
-    'thepolice',
-    'linkinpark',
-    'thelastshadowpuppets',
-    'oasis',
-    'thewho',
-    'bonjovi',
-  ]
+  // const [music, setMusic] = useState([]);
+  const dispatch = useDispatch();
+  const music = useSelector((state) => state.music);
+  const search = useSelector((state) => state.search);
+  const search_music = useSelector((state) => state.music_search[0]);
 
-  let popArtists = [
-    'maroon5',
-    'coldplay',
-    'harrystyles',
-    'taylorswift',
-    'katyperry',
-    'arianagrande',
-  ]
+  let rockArtists = ["queen", "jimi hendrix", "ac/dc"];
+  let popArtists = ["maroon5", "coldplay", "onerepublic"];
+  let hipHopArtists = ["eminem", "snoopdogg", "lilwayne"];
 
-  let hipHopArtists = [
-    'eminem',
-    'snoopdogg',
-    'lilwayne',
-    'drake',
-    'kanyewest',
-  ]
-  const rockAlbums = FetchAlbumsData(rockArtists);
-  const popAlbums = FetchAlbumsData(popArtists);
-  const hiphopAlbums = FetchAlbumsData(hipHopArtists);
+  let headers = new Headers({
+    // sets the headers
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    "X-RapidAPI-Key": "9d408f0366mshab3b0fd8e5ecdf7p1b09f2jsne682a1797fa0",
+  });
 
-  const results = useSelector((state) => state.search.searchResults);
+  const handleArtist = async (artistName) => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/deezer/search?q=" +
+          artistName,
+        {
+          method: "GET",
+          headers,
+        }
+      ); 
+      if (response.ok) {
+        let result = await response.json(); // transforms the response to json
+        let songInfo = result.data;
+        console.log(songInfo);
+        dispatch({
+          type: "ADD_MUSIC",
+          payload: songInfo.filter((el, i) => i < 4),
+        });
+      } else {
+        console.log("error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSearch = async (artistName) => {
+    // artistName = "eminem", "metallica", etc...
+    // domQuerySelector = "#rockSection" etc...
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/deezer/search?q=" +
+          artistName,
+        {
+          method: "GET",
+          headers,
+        }
+      ); // gets the information
+      if (response.ok) {
+        let result = await response.json(); // transforms the response to json
+        let songInfo = result.data;
+        console.log(songInfo);
+        dispatch({
+          type: "ADD_MUSIC_SEARCH",
+          payload: songInfo.filter((el, i) => i < 8),
+        });
+      } else {
+        console.log("error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    rockArtists.map((el) => handleArtist(el));
+    popArtists.map((el) => handleArtist(el));
+    rockArtists.map((el) => handleArtist(el) )
+
   
+  }, []);
+
+  useEffect(() => {
+    handleSearch(search);
+  }, [search]);
 
   return (
-    <Row className="col-12 col-md-9" id="mainPage">
-      <SearchResults results={results} />
-      <AlbumList title="Rock Classics" albums={rockAlbums} />
-      <AlbumList title="Pop Culture" albums={popAlbums} />
-      <AlbumList title="#HipHop" albums={hiphopAlbums} />
-    </Row>
+    <>
+      <Col className="col-12 col-md-8 offset-md-3 mainPage">
+  
+
+        {search_music && (
+          <Row className="row m-0">
+            <Col className="col-10">
+              <div id="searchResults">
+                <h2>Search Results</h2>
+                <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3">
+                  {search_music.map((el, i) => (
+                    <div
+                      key={`music-${i}`}
+                      className="col text-center"
+                      id={el?.id}
+                    >
+                      <a href={`/album/${el?.album?.id}`}>
+                        <img
+                          className="img-fluid"
+                          src={el?.album?.cover_medium}
+                          alt="1"
+                        />
+                      </a>
+                      <p>
+                        <a href={`/album/${el?.album?.id}`}>
+                          {el?.album?.title?.length < 16
+                            ? el?.album?.title
+                            : el?.album?.title.substring(0, 16)}
+                        </a>
+                        <br></br>
+                        <a href={`/artist/${el?.artist?.id}`}>
+                          {el?.artist?.name}
+                        </a>
+                      </p>
+                    </div>
+                  ))}
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        )}
+
+        <Row className="row m-0">
+          <Col className="col-10 p-0">
+            <div id="rock">
+              <h2>Rock classics</h2>
+              <Row
+                className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                id="rockSection"
+              >
+                {music
+                  ?.filter((el, i) => i < 8)
+                  .map((el, i) => (
+                    <div
+                      key={`music-${i}`}
+                      className="col text-center"
+                      id={el?.id}
+                    >
+                      <a href={`/album/${el?.album?.id}`}>
+                        <img
+                          className="img-fluid"
+                          src={el?.album?.cover_medium}
+                          alt="1"
+                        />
+                      </a>
+                      <p>
+                        <a href={`/album/${el?.album?.id}`}>
+                          {el?.album?.title?.length < 16
+                            ? el?.album?.title
+                            : el?.album?.title.substring(0, 16)}
+                        </a>
+                        <br></br>
+                        <a href={`/artist/${el?.artist?.id}`}>
+                          {el?.artist?.name}
+                        </a>
+                      </p>
+                    </div>
+                  ))}
+              </Row>
+            </div>
+          </Col>
+        </Row>
+        <Row className="row m-0">
+          <Col className="col-10  p-0">
+            <div id="pop">
+              <h2>Pop Culture</h2>
+              <Row
+                className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                id="popSection"
+              >
+                {music
+                  ?.filter((el, i) => i > 12 && i < 25)
+                  .map((el, i) => (
+                    <div
+                      key={`pop-${i}`}
+                      className="col text-center"
+                      id={el?.id}
+                    >
+                      <a href={`/album/${el?.album?.id}`}>
+                        <img
+                          className="img-fluid"
+                          src={el?.album?.cover_medium}
+                          alt="1"
+                        />
+                      </a>
+                      <p>
+                        <a href={`/album/${el?.album?.id}`}>
+                          {el?.album?.title?.length < 16
+                            ? el?.album?.title
+                            : el?.album?.title.substring(0, 16)}
+                        </a>
+                        <br></br>
+                        <a href={`/artist/${el?.artist?.id}`}>
+                          {el?.artist?.name}
+                        </a>
+                      </p>
+                    </div>
+                  ))}
+              </Row>
+            </div>
+          </Col>
+        </Row>
+        <Row className="row m-0">
+          <Col className="col-10  p-0">
+            <div id="hiphop">
+              <h2>#HipHop</h2>
+              <Row
+                className="row m-0 row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
+                id="hipHopSection"
+              ></Row>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </>
   );
 };
 
