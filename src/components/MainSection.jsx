4dc8fbc81d7b91b64c1a0c8f072a0610c673dd3d
Damaride Putignano <damarideputignano@gmail.com>
2024-03-29 15:07:08 +0100

    Added fetch & redux

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,53 +1,50 @@
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import AlbumList from "./AlbumList";
+import FetchAlbumsData from "./FetchAlbumsData";
+import { useSelector } from "react-redux";
 
-const MainSection = function () {
+const MainSection = () => {
+  let rockArtists = [
+    'queen',
+    'arcticmonkeys',
+    'thepolice',
+    'linkinpark',
+    'thelastshadowpuppets',
+    'oasis',
+    'thewho',
+    'bonjovi',
+  ]
+
+  let popArtists = [
+    'maroon5',
+    'coldplay',
+    'harrystyles',
+    'taylorswift',
+    'katyperry',
+    'arianagrande',
+  ]
+
+  let hipHopArtists = [
+    'eminem',
+    'snoopdogg',
+    'lilwayne',
+    'drake',
+    'kanyewest',
+  ]
+  const rockAlbums = FetchAlbumsData(rockArtists);
+  const popAlbums = FetchAlbumsData(popArtists);
+  const hiphopAlbums = FetchAlbumsData(hipHopArtists);
+
+  const results = useSelector((state) => state.search.searchResults);
+  
+
   return (
-    <>
-      
-        <Row>
-        <Col>
-          <div id="searchResults" style={{ display: "none" }}>
-            <h2>Search Results</h2>
-            <Row className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"></Row>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        {" "}
-        <Col>
-          <div id="rock">
-            <h2>Rock Classics</h2>
-            <Row
-              className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-              id="rockSection"
-            ></Row>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div id="pop">
-            <h2>Pop Culture</h2>
-            <Row
-              className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-              id="popSection"
-            ></Row>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div id="hiphop">
-            <h2>#HipHop</h2>
-            <Row
-              className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3"
-              id="hipHopSection"
-            ></Row>
-          </div>
-        </Col>
-      </Row>
-      
-    </>
+    <Row className="col-12 col-md-9" id="mainPage">
+      <SearchResults results={results} />
+      <AlbumList title="Rock Classics" albums={rockAlbums} />
+      <AlbumList title="Pop Culture" albums={popAlbums} />
+      <AlbumList title="#HipHop" albums={hiphopAlbums} />
+    </Row>
   );
 };
 
